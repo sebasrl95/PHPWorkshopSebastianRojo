8de54e92f5767d984936e6de5c6a0b22751be39d
Sebastian Rojo <sebastian.rojo@arkix.com>
2017-03-09 15:30:38 -0500

    Configured database and added bootstrap, handlebars scripts

---
+++ CHANGELOG.md
@@ -0,0 +1,29 @@
+##########
+Change Log
+##########
+
+Version 1.2
+===========
+
+Date: March 9, 2017
+
+Version 1.1
+================
+
+Date: March 8, 2017
+
+- Added default controller
+- Added default view
+- Added article model
+- Configured database
+- Removed index.php from the url
+- Added jQuery
+- Added bootstrap
+- Added handlebars
+
+Version 1.0
+================
+
+Release Date: March 07, 2017
+
+First publicly released version.
---
+++ application/models/articlemodel.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ArticleModel extends CI_Model {
+
+     public function getArticles()
+     {
+        return $this->db->get('tb_articles');
+     }
+
+     public function getArticleByID($id = -1)
+     {
+     	$result = $this->db->query('SELECT * FROM tb_articles WHERE id_article = '.$id);
+	    return $result;
+     }
+
+     public function insert($post = null) {
+         if ($post != null) {
+            $title = $post['title'];
+            $description = $post['description'];
+            $file_name = $post['file_name'];
+
+            $data = array(
+                'title' => $title ,
+                'description' => $description,
+                'image_url' => $file_name
+            );
+
+            if ($this->db->insert('tb_articles', $data)) {
+                return true;
+            } 
+         } else {
+             return false;
+         }
+     }
+}
